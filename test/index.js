7e396cbe6299f296076f5659084d0de6125f5fcd
simonhaenisch <simonhaenisch@users.noreply.github.com>
2019-05-25 02:58:31 +0200

    test: add virtual module test

--- test/index.js
+++ test/index.js
@@ -1,18 +1,18 @@
+#!/usr/bin/env node
+
 const { strictEqual } = require('assert');
 const { resolve } = require('path');
 const { resolveTypescriptPaths } = require('../dist');
 
 const plugin = resolveTypescriptPaths({ tsConfigPath: resolve(__dirname, 'tsconfig.json') });
 
-const resolvedBasic = plugin.resolveId('@foobar', '');
-const resolvedWildcard = plugin.resolveId('@bar/foo', '');
-const resolvedJs = plugin.resolveId('@js', '');
-
 try {
 	strictEqual(plugin.resolveId('@asdf', ''), null);
-	strictEqual(resolvedBasic, 'test/foo/bar.ts');
-	strictEqual(resolvedWildcard, 'test/bar/foo.ts');
-	strictEqual(resolvedJs, 'test/js/index.js');
+	strictEqual(plugin.resolveId('\0@foobar', ''), null);
+	strictEqual(plugin.resolveId('@foobar', ''), 'test/foo/bar.ts');
+	strictEqual(plugin.resolveId('@bar/foo', ''), 'test/bar/foo.ts');
+	strictEqual(plugin.resolveId('@js', ''), 'test/js/index.js');
+
 	console.log('PASSED');
 } catch (error) {
 	throw error;
