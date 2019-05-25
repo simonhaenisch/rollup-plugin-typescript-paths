8accca35d5851ad0f6b3d2956a2d2f533e25c635
simonhaenisch <simonhaenisch@users.noreply.github.com>
2019-05-25 02:03:33 +0200

    test: add wildcard and js tests

--- test/index.js
+++ test/index.js
@@ -4,10 +4,15 @@ const { resolveTypescriptPaths } = require('../dist');
 
 const plugin = resolveTypescriptPaths({ tsConfigPath: resolve(__dirname, 'tsconfig.json') });
 
-const resolved = plugin.resolveId('@foobar', '');
+const resolvedBasic = plugin.resolveId('@foobar', '');
+const resolvedWildcard = plugin.resolveId('@bar/foo', '');
+const resolvedJs = plugin.resolveId('@js', '');
 
 try {
-	strictEqual(resolved, 'test/foo/bar.js');
+	strictEqual(plugin.resolveId('@asdf', ''), null);
+	strictEqual(resolvedBasic, 'test/foo/bar.ts');
+	strictEqual(resolvedWildcard, 'test/bar/foo.ts');
+	strictEqual(resolvedJs, 'test/js/index.js');
 	console.log('PASSED');
 } catch (error) {
 	throw error;
