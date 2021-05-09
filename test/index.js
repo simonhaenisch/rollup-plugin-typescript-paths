baa716ac6e7c6fd914f60c697e5a8eef6b193aa5
benjozork <benjamin.dupont.984@gmail.com>
2021-05-08 20:09:24 -0400

    feat: add `preserveExtensions` option

--- test/index.js
+++ test/index.js
@@ -9,17 +9,23 @@ const transform = (path) => path.replace(/\.js$/i, '.cjs.js');
 const plugin = typescriptPaths({ tsConfigPath: resolve(__dirname, 'tsconfig.json') });
 const pluginNonAbs = typescriptPaths({ tsConfigPath: resolve(__dirname, 'tsconfig.json'), absolute: false });
 const pluginTransform = typescriptPaths({ tsConfigPath: resolve(__dirname, 'tsconfig.json'), transform });
+const pluginPreserveExtensions = typescriptPaths({ tsConfigPath: resolve(__dirname, 'tsconfig.json'), preserveExtensions: true });
 
 try {
 	strictEqual(plugin.resolveId('@asdf', ''), null);
 	strictEqual(plugin.resolveId('\0@foobar', ''), null);
 	strictEqual(plugin.resolveId('@foobar', ''), join(__dirname, 'foo', 'bar.js'));
+	strictEqual(plugin.resolveId('@foobar-react', ''), join(__dirname, 'foo', 'barReact.js'));
 	strictEqual(plugin.resolveId('@bar/foo', ''), join(__dirname, 'bar', 'foo.js'));
 	strictEqual(plugin.resolveId('@js', ''), join(__dirname, 'js', 'index.js'));
 
 	strictEqual(pluginNonAbs.resolveId('@foobar', ''), join('test', 'foo', 'bar.js'));
 
 	strictEqual(pluginTransform.resolveId('@foobar', ''), join(__dirname, 'foo', 'bar.cjs.js'));
+
+	strictEqual(pluginPreserveExtensions.resolveId('@foobar', ''), join(__dirname, 'foo', 'bar.ts'));
+	strictEqual(pluginPreserveExtensions.resolveId('@foobar-react', ''), join(__dirname, 'foo', 'barReact.tsx'));
+
 	console.log('PASSED');
 } catch (error) {
 	throw error;
